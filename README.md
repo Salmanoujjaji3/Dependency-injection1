07ea5d9718effd4cbe1f4e1f0a2f92202227d28a
Salmanoujjaji3 <salmanoujjaji@gmail.com>
2024-10-05 02:36:54 +0100

    Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1> Compte rendu du premier TP : Injection des dépendenses</h1>
+<h2> Diagramme de classe : </h2>
+<img src="Captures/1.png">
+<h2> Résultat d'exécution</h2>
+
--- src/main/java/Presentation/Pres1.java
+++ src/main/java/Presentation/Pres1.java
@@ -6,7 +6,8 @@ import ext.DaoImplementV2;
 
 public class Pres1 {
     public static void main(String[] args) {
-        DaoImplementV2 d = new DaoImplementV2(); // manière statique car je connais déja la classe que je veux instancier
+        DAO.DaoImplement d = new DaoImplement(); // manière statique car je connais déja la classe que je veux instancier
+        //DaoImplementV2 d = new DaoImplementV2(); // manière statique car je connais déja la classe que je veux instancier
         IMetierImplement metier = new IMetierImplement(d);//Injection des dépendances via le constructeur
         //metier.setDao(d);// injection des dépendences
         System.out.println("Res = "+metier.calcul());
